5919157b2085d37eacba6cf23dae619aad8d9a84
Gubs The Second <81638433+GubsTheSecond@users.noreply.github.com>
2021-05-27 12:33:57 -0700

    Update bot.py
    I turned it into my bot, Darth Carrot

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
 import discord
 from discord.ext import commands
 
-client=commands.Bot(command_prefix='!')
+client=commands.Bot(command_prefix='$')
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    print("Bumbumbumbumbabumbumbabum")
 
 @client.event
 async def on_member_join(member):
-    print("A wild",member,"appeared")
+    print("We would be honored, if you would join us,",member,".")
 
 @client.event
 async def on_member_remove(member):
-    print("The wild",member,"disappeared")
+    print("I find your lack of faith disturbing, ",member,".")
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"Hey! I am up and running with a ping of {round(client.latency*1000)}ms.")
+    await ctx.send(f"{round(client.latency*1000)}ms.")
 
-client.run('')   #Place token between the ''
+client.run('ODI1MDcyNjIyMzY1OTY2MzM4.YF4mwg.0oydGKRtwR9XLouxkMXjN-YHuy0')   #Place token between the ''
